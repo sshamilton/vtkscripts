3c72ca4546af7506dbfc22fc4f49a02cbf0ecec5
Stephen Hamilton <stephenshamilton@gmail.com>
2016-08-30 15:01:28 -0400

    zfp mod update

--- simclient.py
+++ simclient.py
@@ -70,12 +70,16 @@ while True:
                 args = []
                 modpool = Pool(p["maxpool"])
                 #modpool = Pool(1)
-                for i in range(1,p["numcubes"]):
+                for i in range(p["cube_start"], (p["cube_end"]+1)):
                     args.append([p, i]) #Setup args for each parallel run
+                    print("I is ", i)
+                
                 if p["ptype"] == 1: #Request to do some work.
                     if p["action"] == 1: #compress using ZFP
                         pool_results = modpool.map(zfpcompress, args)
                     elif p["action"] == 2: #vorticity mesh vtk
+                        result = True
+                    elif p["action"] == 3: #vorticity thresh dilated velocity volume
                         result = True
                     elif p["action"] == 4:                        
                         pool_results = modpool.map(testmod, args)
