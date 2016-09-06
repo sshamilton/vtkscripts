3564386b13f9cb81b0492598b8ee8538d7f43806
Stephen hamilton <ssh@jhu.edu>
2016-09-06 12:19:47 -0600

    la database update4

--- myrcene/myrcene/settings.py
+++ myrcene/myrcene/settings.py
@@ -41,16 +41,17 @@ INSTALLED_APPS = (
     'fec',
  
 )
-
+ 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'myrcene.disable.DisableCSRF',
 )
 
 ROOT_URLCONF = 'myrcene.urls'
